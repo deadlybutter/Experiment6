b5c366ef1ec550cf3f42ee8f2fbf0e7fdeb9040a
Joe Kent <thedeadlybutter@gmail.com>
2014-08-09 13:57:35 -0400

    Start of node system

---
+++ Assets/scripts/EntityNode.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EntityNode : Node {
+
+	// Use this for initialization
+	protected override Start () {
+		base.Start();
+	}
+	
+	// Update is called once per frame
+	protected override Update () {
+		base.Update();
+	}
+}
